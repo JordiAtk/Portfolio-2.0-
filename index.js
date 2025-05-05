0863b6256cca76ca3f82320aa0db1c70e127f8d0
Jordan <jordan.atk00@gmail.com>
2025-05-05 14:51:52 -0400

    email system implemented

--- index.js
+++ index.js
@@ -4,23 +4,22 @@
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    // .sendForm(
-    //     'service_w55qni7',
-    //     'template_zpy33oh',
-    //     event.target,
-    //     'FxsojxRC2otipimqB'
-    // ).then(() => {
-    //     console.log('this worked1')
-    // })
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
     loading.classList += " modal__overlay--visible"
-    setTimeout(() => {
-        console.log('it worked 1');
-    }, 500);
-
-    
-
-    
+    emailjs
+    .sendForm(
+        'service_w55qni7',
+        'template_zpy33oh',
+        event.target,
+        'FxsojxRC2otipimqB'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert (
+            "The email service is temporarily unavailable. Contact me directly at jordan.atk00@gmail.com"
+        );
+    })
 }
